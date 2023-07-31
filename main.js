f7edb357b339fe5d4fdbc639f30d718f2ca29373
Mohamed Hany <mohamedhany61@gmail.com>
2023-07-31 20:52:26 +0300

    create evaluate function
    create functions (evaluate , addMinus, MultiplyDivide, removeSignsFromEnd) , update clearScreen to clear output

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-let screen = document.querySelector(".screen");
+let screen = document.querySelector(".screen .input");
+let screenOutput = document.querySelector(".screen .output");
+let arithmeticSigns = ["+", "-", "*", "/"];
 
 // create numberss
 for (i = 1; i < 10; i++) {
@@ -17,13 +19,14 @@ buttons.forEach((btn) => {
   if (btn.innerText == "Ac") {
     btn.addEventListener("click", clearScreen);
   } else if (btn.innerText == "=") {
-    btn.addEventListener("click", evaluate);
+    btn.addEventListener("click", function () {
+      evaluate();
+    });
   } else btn.addEventListener("click", addToScreen);
 });
 
 // functions
 function addToScreen() {
-  let arithmeticSigns = ["+", "-", "*", "/"];
   let previousInput = screen.innerText.slice(-1);
   if (
     arithmeticSigns.includes(this.innerText) ||
@@ -36,18 +39,68 @@ function addToScreen() {
 }
 function clearScreen() {
   screen.innerText = "";
+  screenOutput.innerText = "";
 }
 function add(a, b) {
-  return a + b;
+  return parseFloat(a) + parseFloat(b);
 }
 function minus(a, b) {
-  return a - b;
+  return parseFloat(a) - parseFloat(b);
 }
 function multiply(a, b) {
-  return a * b;
+  return parseFloat(a) * parseFloat(b);
 }
 function divide(a, b) {
-  return a / b;
+  if (b == 0) {
+    screenOutput.innerText = "Divide by 0 !!!";
+  }
+  return parseFloat(a) / parseFloat(b);
 }
 
-function evaluate() {}
+function evaluate(input = screen.innerText) {
+  let inputsArr = input.split(" ");
+  removeSignsFromEnd(inputsArr);
+  inputsArr = multiplyDivide(inputsArr);
+  inputsArr = plusMinus(inputsArr);
+  console.log(inputsArr);
+  screenOutput.innerText = inputsArr[0];
+}
+
+function multiplyDivide(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == "*") {
+      arr[i + 1] = multiply(arr[i - 1], arr[i + 1]);
+      arr.splice(i - 1, 2);
+      i--;
+    } else if (arr[i] == "/") {
+      arr[i + 1] = divide(arr[i - 1], arr[i + 1]);
+      arr.splice(i - 1, 2);
+      i--;
+    }
+  }
+  return arr;
+}
+
+function plusMinus(arr) {
+  let length = arr.length;
+  for (let i = 0; i < length; i++) {
+    if (arr[i] == "+") {
+      arr[i + 1] = add(arr[i - 1], arr[i + 1]);
+      arr.splice(i - 1, 2);
+      i--;
+    } else if (arr[i] == "-") {
+      arr[i + 1] = minus(arr[i - 1], arr[i + 1]);
+      arr.splice(i - 1, 2);
+      i--;
+    }
+  }
+  return arr;
+}
+
+function removeSignsFromEnd(inputsArr) {
+  if (arithmeticSigns.includes(inputsArr[inputsArr.length - 1])) {
+    inputsArr.pop();
+    screen.innerText = screen.innerText.slice(0, -1);
+    removeSignsFromEnd(inputsArr);
+  }
+}
