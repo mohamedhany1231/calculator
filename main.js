76234b3d17b074a9cd58ad60aefdd2f2454b11d7
Mohamed Hany <mohamedhany61@gmail.com>
2023-07-31 19:36:20 +0300

    show numbers on screen and create basic  functions
    create functions to add subtract multiply divide 2 numbers and functions to clear or add elements to screen, adjust screen style

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-// create numbers
-const BUTTONS = document.querySelector(".buttons");
+let screen = document.querySelector(".screen");
+
+// create numberss
 for (i = 1; i < 10; i++) {
   let row = document.querySelector(`.row${2 + Math.floor((i - 1) / 3)}`);
   let lastElm = row.lastElementChild;
@@ -8,3 +9,45 @@ for (i = 1; i < 10; i++) {
   button.innerText = i;
   row.insertBefore(button, lastElm);
 }
+
+// register numbers to screen
+let buttons = document.querySelectorAll("button");
+
+buttons.forEach((btn) => {
+  if (btn.innerText == "Ac") {
+    btn.addEventListener("click", clearScreen);
+  } else if (btn.innerText == "=") {
+    btn.addEventListener("click", evaluate);
+  } else btn.addEventListener("click", addToScreen);
+});
+
+// functions
+function addToScreen() {
+  let arithmeticSigns = ["+", "-", "*", "/"];
+  let previousInput = screen.innerText.slice(-1);
+  if (
+    arithmeticSigns.includes(this.innerText) ||
+    arithmeticSigns.includes(previousInput)
+  ) {
+    screen.innerText += ` ${this.innerText} `;
+  } else {
+    screen.innerText += this.innerText;
+  }
+}
+function clearScreen() {
+  screen.innerText = "";
+}
+function add(a, b) {
+  return a + b;
+}
+function minus(a, b) {
+  return a - b;
+}
+function multiply(a, b) {
+  return a * b;
+}
+function divide(a, b) {
+  return a / b;
+}
+
+function evaluate() {}
